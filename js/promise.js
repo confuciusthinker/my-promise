9046cdc826304441dd1ae78c9673a5b8c9ad2cc9
confuciusthinker <1006398378@qq.com>
2021-04-21 22:01:30 +0800

    feat: 增加状态和结果值以及可改变状态和结果值的方法

--- js/promise.js
+++ js/promise.js
@@ -1,10 +1,31 @@
-class Promise {
-    constructor(executor) {
-        
+function Promise(executor) {
+    this.PromiseState = 'pending';
+    this.PromiseResult = null;
+    const _this = this;
+
+    function resolve(data) {
+        // 1.修改状态
+        _this.PromiseState = 'fulfilled';
+        // 2.设置对象结果值
+        _this.PromiseResult = data;
     }
 
-    then(onResolved, onRejected) {
-    
+    function reject(data) {
+        // 1.修改状态
+        _this.PromiseState = 'rejected';
+        // 2.设置对象结果值
+        _this.PromiseResult = data;
     }
+
+    try {
+        // 同步执行【执行器函数】
+        executor(resolve, reject);   
+    } catch (error) {
+        reject(error);
+    }
+}
+
+Promise.prototype.then = function(onResolved, onRejected) {
+
 }
 
